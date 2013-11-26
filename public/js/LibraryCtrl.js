bfc6e06c1c84008707d72c19bfca82e7cb2f3d4e
Truls Skeie <trulsske@iterate.no>
2013-11-26 13:20:13 +0100

    [26] but the design is not finished

--- public/js/LibraryCtrl.js
+++ public/js/LibraryCtrl.js
@@ -4,7 +4,7 @@ angular.module('codesnipzApp')
   .controller('LibraryCtrl', function ($scope, $http) {
 
     $scope.snippetsCollection = [];
-
+    $scope.menuBoolean = {showMenu:false}
     $scope.init = function() {
         console.log ("init");
         $scope.fetchSnippetsCollection();
@@ -17,6 +17,10 @@ angular.module('codesnipzApp')
         }).success(function(data,status){
                 $scope.snippetsCollection = data;
         });
+    };
+
+    $scope.menu = function() {
+        $scope.menuBoolean.showMenu = !$scope.menuBoolean.showMenu;
     };
 });
 
