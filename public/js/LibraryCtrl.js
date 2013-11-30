ceb34126ac84c8ee3d06c40453a487ebcbf211ed
Truls Skeie <trulsske@iterate.no>
2013-11-30 21:50:04 +0100

    [28] finished

--- public/js/LibraryCtrl.js
+++ public/js/LibraryCtrl.js
@@ -1,32 +1,35 @@
 'use strict';
 
 angular.module('codesnipzApp')
-  .controller('LibraryCtrl', function ($scope, $http) {
+    .controller('LibraryCtrl', function($scope, $http) {
 
-    $scope.snippetsCollection = [];
-    $scope.menuBoolean = {showMenu:false}
-    $scope.init = function() {
-        console.log ("init");
-        $scope.fetchSnippetsCollection();
-    };
+        $scope.snippetsCollection = [];
+        $scope.libraryBoolean = {
+            showMenu: false,
+            showSort: false
+        }
+        $scope.init = function() {
+            console.log("init");
+            $scope.fetchSnippetsCollection();
+        };
 
-    $scope.fetchSnippetsCollection = function(){
-        $http({
-            method : 'GET',
-            url : '/snippets/',
-        }).success(function(data,status){
+        $scope.fetchSnippetsCollection = function() {
+            $http({
+                method: 'GET',
+                url: '/snippets/',
+            }).success(function(data, status) {
                 $scope.snippetsCollection = data;
-        });
-    };
+            });
+        };
 
-    $scope.menu = function() {
-        $scope.menuBoolean.showMenu = !$scope.menuBoolean.showMenu;
-    };
-
-    $scope.closeOnClick = function(index) {
-        $scope.snippetsCollection.splice(index,1);
-        
-
-    };
-});
-
+        $scope.menu = function() {
+            $scope.libraryBoolean.showMenu = !$scope.libraryBoolean.showMenu;
+        };
+        $scope.sort = function() {
+            $scope.libraryBoolean.showSort = !$scope.libraryBoolean.showSort;
+        };
+        $scope.closeOnClick = function(index) {
+            $scope.snippetsCollection.splice(index, 1);
+        };
+      
+    });
