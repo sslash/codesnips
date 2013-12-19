a156d4104d7f84309ec0d23b696b08b8f7603854
Henrik Glasø Skifjeld <henrik@Henriks-MacBook-Pro-2.local>
2013-12-19 16:13:36 +0100

    Added directives for onFocus and onBlur.
    Currently placed inside the app.js file, should be moved to directives.js (didn't get that to work properly)

--- public/js/app.js
+++ public/js/app.js
@@ -1,27 +1,50 @@
 'use strict';
 
-angular.module('codesnipzApp', [
+var app = angular.module('codesnipzApp', [
   'ngCookies',
   'ngResource',
   'ngSanitize',
   'ngRoute'
-])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'partials/home.html',
-        controller: 'HomeCtrl'
-      })
-      .when('/add', {
-        templateUrl: 'partials/addView.html',
-        controller: 'AddCtrl'
-      })
-      .when('/library', {
-        templateUrl: 'partials/libraryView.html',
-        controller: 'LibraryCtrl'
+]);
 
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
+app.config(function ($routeProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'partials/home.html',
+      controller: 'HomeCtrl'
+    })
+    .when('/add', {
+      templateUrl: 'partials/addView.html',
+      controller: 'AddCtrl'
+    })
+    .when('/library', {
+      templateUrl: 'partials/libraryView.html',
+      controller: 'LibraryCtrl'
+
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
   });
+
+app.directive('onFocus', function() {
+    return {
+        restrict: 'A',
+        link: function(scope, elm, attrs) {
+            elm.bind('focus', function() {
+                scope.$apply(attrs.onFocus);
+            });
+        }
+    };        
+});
+
+app.directive('onBlur', function() {
+    return {
+        restrict: 'A',
+        link: function(scope, elm, attrs) {
+            elm.bind('blur', function() {
+                scope.$apply(attrs.onBlur);
+            });
+        }
+    };        
+});
